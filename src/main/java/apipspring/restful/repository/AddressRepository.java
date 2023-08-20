12fdc4955edb4fe90650523018d47af5cf55c25e
Apip Kurniawan <davitaja92@gmail.com>
2023-08-20 22:39:03 +0700

    list address API

--- src/main/java/apipspring/restful/repository/AddressRepository.java
+++ src/main/java/apipspring/restful/repository/AddressRepository.java
@@ -5,11 +5,14 @@ import apipspring.restful.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, String> {
 
     Optional<Address> findFirstByContactAndId(Contact contact, String id);
+
+    List<Address> findAllByContact(Contact contact);
 
 }
